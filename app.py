e10a4dabb0166d667036471d79eebd968dbdf351
arishabh30 <rishabhag20@iitk.ac.in>
2024-08-05 00:32:16 +0530

    HTML and CSS added

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # an app is simply an object of the class Flask
 app = Flask(__name__)
 
+
+JOBS = [
+    {
+        'id' : 1,
+        'title' : 'Data Analyst',
+        'location' : 'Bengaluru, India',
+        'salary' : 'Rs.10,00,000'
+    },
+    {
+        'id' : 2,
+        'title' : 'Data Scientist',
+        'location' : 'Delhi, India',
+        'salary' : 'Rs.15,00,000'
+    },
+    {
+        'id' : 3,
+        'title' : 'Backend Engineer',
+        'location' : 'Remote',
+        'salary' : 'Rs.12,00,000'
+    },
+    {
+        'id' : 4,
+        'title' : 'Frontend Engineer',
+        'location' : 'San Francisco',
+        'salary' : '$120,000'
+    }
+]
+
+
 @app.route("/")
-def hello_world():
-    return "Hello, World"
+def hello_rish():
+    return render_template("home.html", 
+                           jobs = JOBS, 
+                           company_name = "Rish")
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", debug=True)
